77bc0f697888977969cf7db52d4366e861befa23
Krishnendu Mandal <krishnendu.mandal89@gmail.com>
2019-08-25 11:51:09 +0530

    Add project files.

---
+++ PassByValueRef/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text;
+
+namespace PassByValueRef
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string m = "A", n = "B";
+      Swap(m, n);
+      Console.WriteLine($"M = {m}, N = {n}");
+
+      StringBuilder sb1 = new StringBuilder("A");
+      StringBuilder sb2 = new StringBuilder("B");
+      Swap(sb1, sb2);
+      Console.WriteLine($"{sb1.ToString()} {sb2.ToString()}");
+
+      StringBuilder buildText = new StringBuilder();
+      AddSomeText(buildText);
+      Console.WriteLine(buildText.ToString());
+
+      string myText = "Hello";
+      ModifyText(myText);
+      Console.WriteLine(myText);
+    }
+
+    private static void ModifyText(string myText)
+    {
+      myText = myText + " morining";
+    }
+
+    private static void AddSomeText(StringBuilder buildText)
+    {
+      buildText.Append("Hello ");
+      buildText = null;
+    }
+
+    private static void Swap(StringBuilder sb1, StringBuilder sb2)
+    {
+      StringBuilder t = sb1;
+      sb1 = sb2;
+      sb2 = t;
+    }
+
+    private static void Swap(string m, string n)
+    {
+      string t = m;
+      m = n;
+      n = t;
+    }
+  }
+}
